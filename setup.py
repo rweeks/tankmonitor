883fd7f95599fc2414a65616f6d3a654b510c682
Liam Weeks <lweeks@newbrightidea.com>
2023-07-19 12:52:12 -0700

    finished setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
+"""
+The setup.py file is a convention used by Python programmers. The command
+```python setup.py install``` will install the required dependencies that are
+not affiliated with the Raspberry Pi.
+"""
+
 setup(
     name="tankmonitor",
-    packages=[
-        "sockjs-tornado"
+    url='https://github.com/rweeks/tankmonitor',
+    packages=['tankmonitor'],
+    install_requires=[
+        'sockjs-tornado',
+        'pillow',
+        'netifaces'
     ]
 )
 
